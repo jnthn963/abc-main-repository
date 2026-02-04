e31524bf1d18ebbfc0e7a8ea44949a39f2e3d3a8
JONATHAN <nangkiljonathan@gmail.com>
2026-02-05 03:02:13 +0800

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  // Added base path to match your Hostinger subfolder
   base: '/ABC/', 
   plugins: [react()],
   test: {
