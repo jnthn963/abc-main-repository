d5b7a51510d1f5354a643e6a1722351cbf1c639a
JONATHAN <nangkiljonathan@gmail.com>
2026-02-05 01:49:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/ABC-TECH/', // This tells the app its new home is /ABC-TECH/
   server: {
     host: "::",
     port: 8080,
